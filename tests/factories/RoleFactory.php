5ee6520502f1a4f74ffbdb9728a952802a0a0b45
Warrick Bayman <me@warrickbayman.co.za>
2020-03-08 20:09:15 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/factories/RoleFactory.php
+++ tests/factories/RoleFactory.php
@@ -3,8 +3,6 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
-use TPG\Tests\User;
 
 $factory->define(\TPG\Tests\Role::class, function (Faker $faker) {
     return [
